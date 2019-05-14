22080252c6a3023707b166956a222895b1406bf9
Lloyd Chambrier <lloydchambrier@gmail.com>
2019-05-14 15:49:37 +0200

    Rendered user data  on Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,14 +10,22 @@ class Profile extends Component {
         this.logout = this.logout.bind(this)
         
     }
-    componentDidMount(){
-        var user = fire.auth().currentUser;
-        this.setState({ user });
-    }
+    
     logout(){
         fire.auth().signOut();
     }
     render(){
+        var user = fire.auth().currentUser;
+        var name, email, photoUrl, uid, emailVerified;
+
+        if (user != null) {
+            name = user.displayName;
+            email = user.email;
+            photoUrl = user.photoURL;
+            emailVerified = user.emailVerified;
+            uid = user.uid;  
+
+        }
         return(
             <div className="Profile">
                 <div className="Profile_Yoga_List">
@@ -26,7 +34,7 @@ class Profile extends Component {
                 </header>
                 </div>
                 <div className="Profile_Side">
-                    <h1>Welcome to your profile</h1>
+                    <h1>Welcome to your profile {email}</h1>
                     <button onClick={this.logout} className="PageSwitcher__Item">Logout</button>
                 </div>
             </div>
