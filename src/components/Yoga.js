41b77ea50ca31bd53c4789ea7bd371e0dcc03efd
Gábor Kozár <id@gaborkozar.me>
2019-05-28 12:52:16 +0200

    added package react-collapsible

--- src/components/Yoga.js
+++ src/components/Yoga.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
+import Collapsible from 'react-collapsible';
 
 class Yoga extends React.Component {
     render(){
         const { name, description, benefits, image } = this.props.details;
         return (
             <div className="yoga">
-                <h1>{name}</h1>
-                <img src={image} alt="yoga pic" width="100px" height="100px"></img>
-                <br></br>
-                <span>{benefits}</span>
-                <p>{description}</p>
+                <Collapsible trigger={name}>
+                    <img src={image} alt="yoga pic" width="100px" height="100px"></img>
+                    <br></br>
+                    <span>{benefits}</span>
+                    <p>{description}</p>
+                </Collapsible>
             </div>
+        
         )
     }
 }
