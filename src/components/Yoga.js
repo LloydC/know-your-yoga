4f1811eb4f6884351e94880d2bbab1b94e655cca
Gábor Kozár <id@gaborkozar.me>
2019-05-28 16:03:05 +0200

    added functional yoga click effect

--- src/components/Yoga.js
+++ src/components/Yoga.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Collapsible from 'react-collapsible';
 
 class Yoga extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {isBoxVisible: false};
+  }
+    toggleBox = () => {
+  this.setState(prevState => ({ isBoxVisible: !prevState.isBoxVisible }));
+  console.log("working!")
+};
     render(){
         const { name, description, benefits, image } = this.props.details;
+        const { isBoxVisible } = this.state;
         return (
-            <div className="yoga">
-                <Collapsible trigger={name}>
+            <div className={`yoga${isBoxVisible ? " visible" : " hidden"}`} onClick={this.toggleBox}>
+                <Collapsible trigger={name} >
                     <img src={image} alt="yoga pic" width="100px" height="100px"></img>
                     <br></br>
                     <span>{benefits}</span>
