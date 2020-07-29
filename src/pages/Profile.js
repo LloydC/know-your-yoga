f79a2e9df7b167c9313fc4213da8e10725654d08
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-07-29 18:21:49 +0200

    did some changes to renovate.json

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -25,54 +25,53 @@ class Profile extends Component {
 
         return ( <
             div className = "Profile" >
-            // <
-            // div className = "Profile_Yoga_List" >
-            // <
-            // header className = "Profile-header" >
-            // <
-            // h1 onClick = {
-            //     this.renderMarker
-            // } > Know Your Yoga < /h1> <
-            // div className = "User_Info" >
-            // <
-            // ul className = "yogalist" > {
-            //     Object.keys(this.state.yoga_list).map(key =>
-            //         <
-            //         Yoga key = {
-            //             key
-            //         }
-            //         index = {
-            //             key
-            //         }
-            //         details = {
-            //             this.state.yoga_list[key]
-            //         }
-            //         renderMarker = {
-            //             this.renderMarker
-            //         }
-            //         />
-            //     )
-            // } <
-            // /ul> 
-            // < /
-            // div > <
-            // /header> < /
-            // div > <
-            // div className = "Profile_Side" >
-            // <
-            // h1 > Welcome to your profile {
-            //     user.email
-            // } < /h1> <
-            // button onClick = {
-            //     this.logout
-            // }
-            // className = "PageSwitcher__Item" > Logout < /button> <
-            // Map / >
-            // <
-            // YogaForm / >
-            // <
-            // /div>  
             <
+            div className = "Profile_Yoga_List" >
+            <
+            header className = "Profile-header" >
+            <
+            h1 onClick = {
+                this.renderMarker
+            } > Know Your Yoga < /h1> <
+            div className = "User_Info" >
+            <
+            ul className = "yogalist" > {
+                Object.keys(this.state.yoga_list).map(key =>
+                    <
+                    Yoga key = {
+                        key
+                    }
+                    index = {
+                        key
+                    }
+                    details = {
+                        this.state.yoga_list[key]
+                    }
+                    renderMarker = {
+                        this.renderMarker
+                    }
+                    />
+                )
+            } <
+            /ul>  <
+            /
+            div > <
+            /header> < /
+            div > <
+            div className = "Profile_Side" >
+            <
+            h1 > Welcome to your profile {
+                user.email
+            } < /h1> <
+            button onClick = {
+                this.logout
+            }
+            className = "PageSwitcher__Item" > Logout < /button> <
+            Map / >
+            <
+            YogaForm / >
+            <
+            /div>   <
             /
             div >
         )
