4fc205e69c6e9cf4442faffb38fc80f501e5e113
wax-bear <tm.wassenberg@gmail.com>
2020-10-28 12:03:22 +0100

    Make home page responsive, change alignment subtitle, add margin right to signup button and fix typo

--- src/components/HeaderComponent/Header.js
+++ src/components/HeaderComponent/Header.js
@@ -1,21 +1,15 @@
 import React from "react";
-import logo from '../../assets/icons/yoga-icons/color/png/152/yin_yang.png'
+import logo from "../../assets/icons/yoga-icons/color/png/152/yin_yang.png";
 
 export default function Header() {
-    return ( <
-        div className = "App__Aside" >
-        <
-        header className = "App-header" >
-        <
-        h1 > Know Your Yoga < /h1> <
-        img src = {
-            logo
-        }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        span > Curious about Yoga ? Join us and find out where to start < /span> < /
-        header > <
-        /div>
-    );
+  return (
+    <div className="App__Aside">
+      <header className="App-header">
+        <h1> Know Your Yoga </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <span>Curious about Yoga ?</span>
+        <span>Join us and find out where to start</span>
+      </header>
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,61 +1,58 @@
-import React, {
-    Component
-} from 'react';
-import {
-    Route,
-    NavLink
-} from 'react-router-dom';
-import Header from '../components/HeaderComponent/Header'
-import SignUpForm from './SignUpForm';
-import SignInForm from './SignInForm';
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+import Header from "../components/HeaderComponent/Header";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
 class Home extends Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            Header / >
-            <
-            div className = "App__Form" >
-            <
-            div className = "PageSwitcher" >
-            <
-            NavLink to = "/sign-in"
-            activeClassName = "PageSwitcher__Item--Active"
-            className = "PageSwitcher__Item" > Sign In < /NavLink> <
-            NavLink exact to = "/"
-            activeClassName = "PageSwitcher__Item--Active"
-            className = "PageSwitcher__Item" > Sign Up < /NavLink> <
-            /div>
-
-            <
-            div className = "FormTitle" >
-            <
-            NavLink to = "/sign-in"
-            activeClassName = "FormTitle__Link--Active"
-            className = "FormTitle__Link" > Sign In < /NavLink> or <NavLink exact to="/
-            " activeClassName="
-            FormTitle__Link--Active " className="
-            FormTitle__Link ">Sign Up</NavLink> <
-            /div>
-
-            <
-            Route exact path = "/"
-            component = {
-                SignUpForm
-            } >
-            <
-            /Route> <
-            Route path = "/sign-in"
-            component = {
-                SignInForm
-            } >
-            <
-            /Route> <
-            /div> <
-            /div>
-        )
-    }
+  render() {
+    return (
+        <div className="App">
+            <div className="Container">
+                <Header />
+                <div className="App__Form">
+                <div className="PageSwitcher">
+                    <NavLink
+                    to="/sign-in"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                    >
+                    Sign In
+                    </NavLink>
+                    <NavLink
+                    exact
+                    to="/"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                    >
+                    Sign Up
+                    </NavLink>
+                </div>
+                <div className="FormTitle">
+                    <NavLink
+                    to="/sign-in"
+                    activeClassName="FormTitle__Link--Active"
+                    className="FormTitle__Link"
+                    >
+                    Sign In
+                    </NavLink>
+                    or
+                    <NavLink
+                    exact
+                    to="/"
+                    activeClassName="FormTitle__Link--Active "
+                    className="FormTitle__Link "
+                    >
+                    Sign Up
+                    </NavLink>
+                </div>
+                <Route exact path="/" component={SignUpForm}></Route>
+                <Route path="/sign-in" component={SignInForm}></Route>
+                </div>
+            </div>
+        </div>
+    );
+  }
 }
 
 export default Home;
