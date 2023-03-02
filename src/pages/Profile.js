2e9e7358af877f53ca367d2017577086b28ccd61
LloydC <lloydchambrier@gmail.com>
2023-03-02 15:31:57 +0000

    ✨Conditional rendering for Yoga map

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,7 +12,8 @@ class Profile extends Component {
         super(props);
         this.state = {
             user: {},
-            yoga_list: yogaStyles
+            yoga_list: yogaStyles,
+            displayContainer: 'yogaStyles'
         }
         this.logout = this.logout.bind(this)
     }
@@ -23,57 +24,39 @@ class Profile extends Component {
     render() {
         const user = fire.auth().currentUser;
 
-        return ( <
-            div className = "Profile" >
-            <
-            div className = "Profile_Yoga_List" >
-            <
-            header className = "Profile-header" >
-            <
-            h1 onClick = {
-                this.renderMarker
-            } > Know Your Yoga < /h1> <
-            div className = "User_Info" >
-            <
-            ul className = "yogalist" > {
-                Object.keys(this.state.yoga_list).map(key =>
-                    <
-                    Yoga key = {
-                        key
+        return ( 
+            <div className="Profile" >
+                <div className="Profile_Yoga_List" >
+                    <header className="Profile-header" >
+                    <h1 onClick= {this.renderMarker}> Know Your Yoga </h1> 
+                    {this.state.displayContainer === 'yogaStyles' ? 
+                    <div className="User_Info" >
+                        <ul className="yogalist" > 
+                            {
+                                Object.keys(this.state.yoga_list).map(key =>
+                                    <Yoga 
+                                        key={key} 
+                                        index={key}
+                                        details= {this.state.yoga_list[key]}
+                                        renderMarker = {this.renderMarker}
+                                    />
+                                )
+                            } 
+                        </ul>  
+                    </div> : 
+                    <Map /> 
                     }
-                    index = {
-                        key
-                    }
-                    details = {
-                        this.state.yoga_list[key]
-                    }
-                    renderMarker = {
-                        this.renderMarker
-                    }
-                    />
-                )
-            } <
-            /ul>  <
-            /
-            div > <
-            /header> < /
-            div > <
-            div className = "Profile_Side" >
-            <
-            h1 > Welcome to your profile {
-                user.email
-            } < /h1> <
-            button onClick = {
-                this.logout
-            }
-            className = "PageSwitcher__Item" > Logout < /button> <
-            Map / >
-            <
-            YogaForm / >
-            <
-            /div>   <
-            /
-            div >
+                    </header> 
+                </div> 
+                <div className = "Profile_Side" >
+                    <h1> Welcome to your profile {user.email} </h1> 
+                    <div>
+                        <button onClick={()=> this.setState({...this.state, displayContainer: this.state.displayContainer === 'yogaStyles' ? "yogaMap" : "yogaStyles"})}>{ this.state.displayContainer !== 'yogaStyles' ? "Display yoga styles" : "Display yoga map"}</button>
+                        <button onClick ={this.logout} className = "PageSwitcher__Item" > Logout </button> 
+                    </div>
+                    <YogaForm />
+                </div>   
+            </div >
         )
     }
 }
